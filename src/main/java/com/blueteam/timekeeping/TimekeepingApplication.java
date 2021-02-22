02f16c6204cc84d2b8996de4f8814f5206926309
Matt <mwolf14@ivytech.edu>
2021-02-22 14:15:41 -0500

    working skeleton

---
+++ src/main/java/com/blueteam/timekeeping/TimekeepingApplication.java
@@ -0,0 +1,13 @@
+package com.blueteam.timekeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimekeepingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimekeepingApplication.class, args);
+	}
+
+}
