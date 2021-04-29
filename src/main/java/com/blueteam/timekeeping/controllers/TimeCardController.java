ee94c892d38ee0cedfecab49ca55333c168fe07c
Matt <mwolf14@ivytech.edu>
2021-04-29 10:05:49 -0400

    fixing things....

--- src/main/java/com/blueteam/timekeeping/controllers/TimeCardController.java
+++ src/main/java/com/blueteam/timekeeping/controllers/TimeCardController.java
@@ -146,4 +146,11 @@ public class TimeCardController {
 		String destinationPage = (msgs.get(2) == "true")? "manager": "employee";
 		return destinationPage;
 	}
+	
+	@GetMapping(path="/correcttimeticket")
+	public String CorrectTimeTicket(Model model, HttpServletRequest request) {
+		
+		
+		return "editpage";
+	}
 }
