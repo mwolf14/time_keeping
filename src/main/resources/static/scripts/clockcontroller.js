0554a1bee04c892f0ac99364a677e96d3464d005
Evan Beach <68313594+ebeach7IVY@users.noreply.github.com>
2021-05-14 17:32:02 -0400

    change submission - not rollback

--- src/main/resources/static/scripts/clockcontroller.js
+++ src/main/resources/static/scripts/clockcontroller.js
@@ -5,45 +5,40 @@ Desc: add the event listener to the clock in clock out button
 "use strict"
 //the next 2 functions will probably need deleted. moved it to an anon in init
 function clockIn(){
-	$('#clockinbtn').on("click", function(){
-			$.ajax({
+	$.ajax({
 		async: false,
 		type: 'GET',
 		url: "/clockin",
 		success: function(data, status) {
 			let btn = document.createElement('button');
 			btn.id = "clockoutbtn";
-			btn.textContent = "Clock Out";
 			btn.addEventListener("click", clockOut, false);
-			let btn_span = document.getElementById("btn_span");
-			btn_span.innerHTML = "";
-			btn_span.appendChild(btn);
+			let clockspan = document.getElementById("btn_span");
+			clockspan.innerHTML = "";
+			btnspan.appendChild(btn);
 			},
 		failure: function(data, status){
 			
 		}
-	})});
+		});
 }
 
 function clockOut(){
-	$('#clockoutbtn').on("click", function(){
-					$.ajax({
+	$.ajax({
 		async: false,
 		type: 'GET',
 		url: "/clockout",
 		success: function(data, status) {
 			let btn = document.createElement('button');
 			btn.id = "clockinbtn";
-			btn.textContent = "Clock In";
 			btn.addEventListener("click", clockIn, false);
-			let btn_span = document.getElementById("btn_span");
-			btn_span.innerHTML = "";
-			btn_span.appendChild(btn);
+			let clockspan = document.getElementById("btn_span");
+			clockspan.innerHTML = "";
+			btnspan.appendChild(btn);
 			},
 			failure: function(data, status){
 				
 			}
-			});
 		});
 }
 
@@ -51,12 +46,44 @@ function init(){
 	console.log("in the init function of the clockcontroller script");
 	if ($('#clockinbtn')){
 		console.log("in the init function clockinbtn");
-    	clockIn();
-		
+    	$('#clockinbtn').on("click", function(){
+			$.ajax({
+		async: false,
+		type: 'GET',
+		url: "/clockin",
+		success: function(data, status) {
+			let btn = document.createElement('button');
+			btn.id = "clockoutbtn";
+			btn.addEventListener("click", clockOut, false);
+			let clockspan = document.getElementById("btn_span");
+			clockspan.innerHTML = "";
+			btnspan.appendChild(btn);
+			},
+		failure: function(data, status){
+			
+		}
+		});
+		});
     } else{
 			console.log("in the init function clockoutbtn");
-    		clockOut();
-	}
+    		$('#clockoutbtn').on("click", function(){
+					$.ajax({
+		async: false,
+		type: 'GET',
+		url: "/clockout",
+		success: function(data, status) {
+			let btn = document.createElement('button');
+			btn.id = "clockinbtn";
+			btn.addEventListener("click", clockIn, false);
+			let clockspan = document.getElementById("btn_span");
+			clockspan.innerHTML = "";
+			btnspan.appendChild(btn);
+			},
+			failure: function(data, status){
+				
+			}
+			});
+    })
 }
 
 //eventlisteners for this page
